b209995ae2f44bd4561c82d0c880ff766f818f13
srikarkarthik-678 <srikarkarthik93@outlook.com>
2025-10-06 02:49:21 +0530

    Update Footer.jsx

--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import { FaInstagram, FaSnapchatGhost, FaTwitter, FaFacebookF } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="bg-black text-gray-600 py-2 border-t border-gray-400/30">
       <div className="flex justify-center space-x-4 mb-3 mt-2">
-        <a href="#" className="border border-gray-300 p-2 rounded-full hover:bg-gray-100 transition">
+        <a href="https://www.instagram.com/aero_mu/" className="border border-gray-300 p-2 rounded-full hover:bg-gray-100 transition">
           <FaInstagram className="text-gray-400 text-lg" />
         </a>
-        <a href="#" className="border border-gray-300 p-2 rounded-full hover:bg-gray-100 transition">
-          <FaSnapchatGhost className="text-gray-400 text-lg" />
+        <a href="https://www.linkedin.com/company/109036026/admin/dashboard/" className="border border-gray-300 p-2 rounded-full hover:bg-gray-100 transition">
+          <FaLinkedin className="text-gray-400 text-lg" />
         </a>
-        <a href="#" className="border border-gray-300 p-2 rounded-full hover:bg-gray-100 transition">
-          <FaTwitter className="text-gray-400 text-lg" />
-        </a>
+
         <a href="#" className="border border-gray-300 p-2 rounded-full hover:bg-gray-100 transition">
           <FaFacebookF className="text-gray-400 text-lg" />
         </a>
-        
+
       </div>
 
       <ul className="flex justify-center space-x-6 mb-2 text-xs font-medium">
@@ -27,11 +26,11 @@ const Footer = () => {
         <li><Link href="/user/project" className="hover:text-gray-300">Projects</Link></li>
         <li><Link href="/user/achivements" className="hover:text-gray-300">Achievements</Link></li>
       </ul>
-      
+
       <p className="text-center text-[15px] text-gray-500 font-title">
         Aero © {new Date().getFullYear()} | Made By SrikarKarthik
       </p>
-      
+
     </footer>
   );
 };
