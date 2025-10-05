35c3c2e679e7899e906fd9e0c021f2f593bbe6f7
srikarkarthik-678 <srikarkarthik93@outlook.com>
2025-10-06 02:50:42 +0530

    Update Hover.jsx

--- app/Components/Hover.jsx
+++ app/Components/Hover.jsx
@@ -8,7 +8,7 @@ const Hover = () => {
                 <div className="hovercontent font-title">
                     <div className="flex justify-between items-center gap-4 ml-10  p-5 rounded-xl w-[100%] max-md:flex max-md:flex-col-reverse max-md:justify-center max-md:items-center max-md:ml-0">
                         <div className='text-gray-400 text-md'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe suscipit unde nobis eaque. Ipsum, reprehenderit pariatur eius provident fugit vero, alias perspiciatis, voluptas sequi adipisci libero nemo veniam blanditiis aliquid. Rem necessitatibus ipsam ratione soluta, sunt natus culpa itaque commodi.</div>
-                        <div className="w-[150vw]  flex justify-center items-center max-md:w-[50vw]">
+                        <div className="w-[150vw]  flex justify-center items-center max-md:w-[70vw]">
                             <Cubes
                                 gridSize={8}
                                 maxAngle={60}
