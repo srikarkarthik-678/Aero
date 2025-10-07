50263114ba0f9abd8dae3da4c964cdbc3d8fdb4c
srikarkarthik-678 <srikarkarthik93@outlook.com>
2025-10-07 21:44:26 +0530

    Update page.jsx

--- app/user/achivements3/page.jsx
+++ app/user/achivements3/page.jsx
@@ -6,14 +6,14 @@ const page = () => {
             <div className="project bg-black text-white font-title">
                 <div className="preojectdetails">
                     <div className="flex justify-center items-center h-[85vh] max-md:h-[55vh] max-sm:h-[47vh]">
-                        <img src="/WhatsApp Image 2025-10-03 at 15.22.29_e94e2435.jpg" alt="" className='w-[700px] rounded-xl' />
+                        <img src="/WhatsApp Image 2025-10-06 at 12.23.56_c8fafd12.jpg" alt="" className='w-[500px] rounded-xl max-md:w-[300px] max-md:mt-20' />
                     </div>
                     <div className='flex flex-col justify-center items-center '>
-                         <div className='p-5 rounded-xl text-gray-400'>
-                            <p className='text-center w-[700px] max-md:w-[400px] max-sm:w-[330px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eum eos odio, nesciunt nulla reprehenderit laboriosam rerum, natus fuga quia rem assumenda! In officia doloremque provident. Ducimus ut fuga eligendi sint corrupti temporibus beatae.</p>
-                            <p className='text-center w-[700px] mt-3 max-md:w-[400px] max-sm:w-[330px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, inventore? Incidunt aperiam, placeat, at consequatur eos porro autem ab maiores tempora cumque similique, a recusandae. Itaque animi temporibus earum voluptas doloremque eveniet, doloribus reprehenderit?</p>
-                            <p className='text-center w-[700px] mt-3 max-md:w-[400px] max-sm:w-[330px]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, soluta! Quae nostrum facilis suscipit, libero, consequatur iusto deleniti exercitationem expedita quos iure voluptas quibusdam, soluta magni voluptates? Culpa voluptatem dolorem minus reprehenderit dicta laborum?</p>
-                            <p className='text-center w-[700px] mt-3 max-md:w-[400px] max-sm:w-[330px]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, soluta! Quae nostrum facilis suscipit, libero, consequatur iusto deleniti exercitationem expedita quos iure voluptas quibusdam, soluta magni voluptates? Culpa voluptatem dolorem minus reprehenderit dicta laborum?</p>
+                        <div className='p-5 rounded-xl text-gray-400'>
+                            <p className='text-center w-[700px] max-md:w-[400px] max-sm:w-[330px] mt-8'> Building on this success, we recently participated in a preparatory workshop for the upcoming DDC event, focusing on both our Micro and Regular class entries. We are excited to report that this workshop was a resounding success. Our lightweight, hand-launched Micro class plane demonstrated exceptional stability and efficiency in the air. Simultaneously, our larger, ground-takeoff Regular class UAV showcased its robust design and powerful performance. Seeing both our aircraft fly successfully has validated our engineering approach and gives us immense confidence as we move into the final stages of preparation for the main competition.</p>
+                            <p className='text-center w-[700px] mt-3 max-md:w-[400px] max-sm:w-[330px]'>Of course. Here is an elaborated paragraph that builds upon the success of the workshop.
+                                The recent preparatory workshop served as the ultimate proving ground for our designs, and we are thrilled with the results. Watching our lightweight Micro class plane being launched by hand and then gracefully cutting through the air with remarkable steadiness was a pivotal moment. Its flight was a clear demonstration of a successful aerodynamic design, validating our focus on efficiency and low-drag construction. In stark contrast, the powerful roar of our Regular class UAV's engine during its confident takeoff run was equally gratifying. It climbed with authority and navigated its flight path precisely</p>
+                           
                         </div>
                     </div>
 
